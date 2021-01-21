ff9ee345065951e4beb88529903c0499fba1e901
s80244 <s80244@beuth-hochschule.de>
2021-01-21 14:04:31 +0100

    commit

--- app/src/main/java/com/example/gpstimer/ShowTimeTableActivity.java
+++ app/src/main/java/com/example/gpstimer/ShowTimeTableActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ShowTimeTableActivity extends AppCompatActivity {
 
@@ -19,5 +21,10 @@ public class ShowTimeTableActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Spinner spinner = findViewById(R.id.spinner);
+        String[] items = new String[]{getString(R.string.sortLowTime), getString(R.string.sortHighTime), getString(R.string.sortMostRec), getString(R.string.sortMostDat)};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, items);
+        spinner.setAdapter(adapter);
     }
 }
